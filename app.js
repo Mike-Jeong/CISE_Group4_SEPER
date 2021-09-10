55456b45469c4e39cee52047ca5b2d7b5de1d9ef
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-09-10 22:04:58 +1200

    first

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 // app.js
 
+// express 모듈 불러오기
 const express = require('express');
 
+// express 객체 생성
 const app = express();
 
-const path = require('path');
-
-app.use('/api/data', function(req, res) {
-  res.json({ greeting: 'Hello World' });
-});
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
+// 기본 포트를 app 객체에 설정
 const port = process.env.PORT || 5000;
 app.listen(port);
 
+// 미들웨어 함수를 특정 경로에 등록
+app.use('/api/data', function(req, res) {
+    res.json({ greeting: 'Hello World' });
+});
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
-  });
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+console.log(`server running at http ${port}`);
