d0894c947cc41ea9816d9564488de0ff02f5d708
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-09-10 21:42:15 +1200

    aa

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const app = express();
 
 const path = require('path');
 
+app.use('/api/data', function(req, res) {
+  res.json({ greeting: 'Hello World' });
+});
+
 app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
+
 
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
