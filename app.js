3bee3fb8e625d3d92fa5e8bf64511abcbcecb5d0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-09-10 21:25:18 +1200

    build

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const express = require('express');
 
 const app = express();
 
+const path = require('path');
+
 app.get('/', (req, res) => res.send('Hello world!'));
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+  });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
