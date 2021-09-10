9731db2940081449240032119f06fab8a31d6c85
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-09-10 22:45:03 +1200

    second

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use('/api/data', function(req, res) {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
 });
 
 console.log(`server running at http ${port}`);
