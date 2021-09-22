f64e7ba68c731f0bca51dbd773186f02d6db240c
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-09-22 18:06:20 +1200

    fas
    adf

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -3,16 +3,20 @@ import Styles from "../components/tablestyle.js";
 import Table from "../components/evidencetable.js";
 import tablecolumns from "../components/tablecolumns.js";
 import Dropdown from "../components/Dropdown.js";
+import axios from 'axios';
+
+
 
 const SEPractice = () => {
     return (
       <div>
         <h2>Select SE Practice to get evidence for the claimed benefits</h2>
         <option value="">Select an SE Practice </option>
+
         <Dropdown/>
         <Styles>
         <Table
-        data={articles}
+        data= {articles}
         columns={tablecolumns}
         />
         </Styles>
