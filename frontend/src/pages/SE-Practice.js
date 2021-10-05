cf35e92fe603120a96b8000d96e4b9b6c5cb433b
JemRC <69658441+JemRC@users.noreply.github.com>
2021-10-05 13:46:10 +1300

    popup message
    committing popup message for form submission

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -21,11 +21,6 @@ const SEPractice = () => {
     .catch(err =>{
       console.log('Error in SE-Practice: Cannot fetch data from database.');
     })
-<<<<<<< HEAD
-    
-  }
-=======
->>>>>>> 8252370f1fd2eca6f3dfd42f162c071a80f15199
 
     return (
       <div>
@@ -43,7 +38,7 @@ const SEPractice = () => {
       </div>
     );
 
-    
+
 }
  
 export default SEPractice;  
