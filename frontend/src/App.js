22670b204d66b4a0a14ac85062f9661c70725eef
Ray Garcia <raycrescent@gmail.com>
2021-10-10 16:25:15 +1300

    Page Redirection Fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import {
   Route,
   NavLink,
   BrowserRouter as Router,
-  Redirect
 } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -12,26 +11,56 @@ import SubmitArticle from "./pages/Submit-Article";
 import NotFoundPage from "./pages/404";
 
 const App = () =>  {
+
+  var CurrentPage;
+  var CurrentNav;
+  
+  if(window.location.pathname === "/"){
+    CurrentPage = Home;
+    CurrentNav = "/";
+  }
+
+  else if(window.location.pathname === "/SEPractice"){
+    CurrentPage = SEPractice;
+    CurrentNav = "/SEPractice";
+  }
+
+  else if(window.location.pathname === "/SubmitArticle"){
+    CurrentPage = SubmitArticle;
+    CurrentNav = "/SubmitArticle";
+  }
+
+  else if(window.location.pathname === "/SubmitArticle"){
+    CurrentPage = SubmitArticle;
+    CurrentNav = "/SubmitArticle";
+  }
+
+  else if(window.location.pathname === "/404"){
+    CurrentPage = NotFoundPage;
+    CurrentNav = "/404";
+  }
+
+  else{
+    CurrentPage = NotFoundPage;
+    CurrentNav = "/404";
+    window.location.pathname = "/404"
+  }
+  
     return (
-        <Router>
+        <Router forceRefresh>
         <div>
           <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
             <ul className="header">
-              <li><NavLink exact to = "/">Home</NavLink></li>
-              <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-              <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+              <li><NavLink to = "/" >Home</NavLink></li>
+              <li><NavLink to = "/SEPractice" >Select the Practice</NavLink></li>
+              <li><NavLink to = "/SubmitArticle" >Submit an Article</NavLink></li>
             </ul>
           <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route  path="/SEPractice" component={SEPractice}/>
-            <Route  path="/SubmitArticle" component={SubmitArticle}/>
-            <Route exact path="/404" component={NotFoundPage}/>          
-            <Redirect to="/404" />
+            <Route exact path={CurrentNav} component={CurrentPage}/>
           </div>
         </div>
-        </Router>
-    );
-
+        </Router>   
+    )
     
 }
  
