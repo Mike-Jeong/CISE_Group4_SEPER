47735e3bfcdf068c6e389f238a643d11139814a6
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-10-11 01:29:48 +1300

    test

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // app.js
 
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
-var cors = require('cors');
 
-const userarticles = require('./routes/api/userarticles')
-const moderarticles = require('./routes/api/moderarticles')
+const userarticles = require('./routes/api/userarticles');
+const moderarticles = require('./routes/api/moderarticles');
+
 const app = express();
 
 connectDB();
 
+// eslint-disable-next-line import/order
 const path = require('path');
 
 app.use(cors({ origin: true, credentials: true }));
@@ -17,13 +19,11 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-//app.get('/', (req, res) => res.send('Hello world!'));
+// app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/userarticles', userarticles);
 app.use('/api/moderarticles', moderarticles);
-
-
 
 const port = process.env.PORT || 5000;
 app.listen(port);
@@ -31,7 +31,7 @@ app.listen(port);
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+  res.sendFile(path.join(`${__dirname}/frontend/build/index.html`));
 });
 
 console.log(`server running at http ${port}`);
