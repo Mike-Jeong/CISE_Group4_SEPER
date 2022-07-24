10ac880fd23e01f5e27fd6d160b36e180bded754
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-24 13:50:02 +0900

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,12 +10,12 @@ import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article";
 import NotFoundPage from "./pages/404";
 
-const App = () =>  {
+const App = () => {
 
   var CurrentPage;
   var CurrentNav;
 
-  switch(window.location.pathname){
+  switch (window.location.pathname) {
     case "/":
       CurrentPage = Home;
       CurrentNav = "/";
@@ -38,22 +38,22 @@ const App = () =>  {
       window.location.pathname = "/404";
   }
 
-    return (
-        <Router forceRefresh>
-        <div>
-          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
-            <ul className="header">
-              <li><NavLink exact to = "/" >Home</NavLink></li>
-              <li><NavLink to = "/SEPractice" >Select the Practice</NavLink></li>
-              <li><NavLink to = "/SubmitArticle" >Submit an Article</NavLink></li>
-    
-            </ul>
-          <div className="content">
-            <Route exact path={CurrentNav} component={CurrentPage}/>
-          </div>
+  return (
+    <Router forceRefresh>
+      <div>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+        <ul className="header">
+          <li><NavLink exact to="/" >Home</NavLink></li>
+          <li><NavLink to="/SEPractice" >Select the Practice</NavLink></li>
+          <li><NavLink to="/SubmitArticle" >Submit an Article</NavLink></li>
+
+        </ul>
+        <div className="content">
+          <Route exact path={CurrentNav} component={CurrentPage} />
         </div>
-        </Router>   
-    )    
+      </div>
+    </Router>
+  )
 
 }
 export default App;
